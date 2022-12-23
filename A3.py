0b198ee1614e02ee3e243cba86fbd4eaacada14d
mohan <mraj03100@gmail.com>
2022-12-23 09:27:47 +0530

    this my second commit

---
+++ A3.py
@@ -0,0 +1,8 @@
+# create a tuple 
+product = ('HP', 'Dell', 499.99)
+
+# access element at index 0
+print(product[2])   # Microsoft
+
+# access element at index 1
+print(product[1])   # Xbox
---
+++ A5.py
@@ -0,0 +1,8 @@
+# create a tuple 
+product = ('Mysql', 'Oracle', 499.99)
+
+# access element at index 0
+print(product[0])   
+
+# access element at index 1
+print(product[1])   
